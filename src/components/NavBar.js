a04ffab86e1eb21e680887d50bbea333f6b85e2b
Leidy Grajales <leidy300196@gmail.com>
2022-07-25 17:17:37 -0500

    Se agrego una carpeta llamada components y se crearon los archivos solicitados en la entrega de la clase 3 (JSX y transpiling)

---
+++ src/components/NavBar.js
@@ -0,0 +1,16 @@
+function NavBar() {
+    return (
+        <header id="header">
+            <h1 className="header__title">Hotel.com</h1>
+            <nav className="header__items">
+                <a href="">home</a>
+                <a href="">Alojamiento</a>
+                <a href="">Reservas</a>
+                <a href="">Contacto</a>
+            </nav>
+        </header>
+    )
+
+}
+
+export default NavBar
